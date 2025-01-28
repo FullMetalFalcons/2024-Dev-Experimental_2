bb31d6899240174dd8fdfa9a9d81c179ecac02db
Scott Tompkins <scot.tompkins@gmail.com>
2025-01-28 12:15:53 -0500

    completes additional tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -78,7 +78,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 0.9177152144435512;
         public double kV = 0.0005874680396104956;
-        public double kA = 0;
+        public double kA = 0.00012;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -90,9 +90,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 10.0;
+        public double lateralGain = 8.0;
+        public double headingGain = 15.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
